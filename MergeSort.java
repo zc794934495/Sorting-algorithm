62857290c09939d0afb00ede54ac4025a2454361
794934495@qq.com <794934495@qq.com>
2020-11-08 17:50:47 +0800

    归并排序

---
+++ MergeSort.java
@@ -0,0 +1,33 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr,int left,int right,int[] temp) {
+        if(left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr,left,mid,temp);
+            mergeSort(arr,mid + 1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+    public static void merge(int[] arr,int left,int mid,int right,int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+        while(i <= mid && j <= right) {
+            if(arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            }else {
+                temp[t++] = arr[j++];
+            }
+        }
+        while(i <= mid) {
+            temp[t++] = arr[i++];
+        }
+        while(j <= right) {
+            temp[t++] = arr[j++];
+        }
+        t = 0;
+        int index = left;
+        while (index <= right) {
+            arr[index++] = temp[t++];
+        }
+    }
+}
