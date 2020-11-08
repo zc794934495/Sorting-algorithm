717a40be775880d20e99229ce05ac34fa16e210a
794934495@qq.com <794934495@qq.com>
2020-11-08 16:25:55 +0800

    选择排序

---
+++ SelectSort.java
@@ -0,0 +1,19 @@
+public class SelectSort {
+    //选择排序
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if(min > arr[j]) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+    }
+}
