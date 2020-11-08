757188a2b53da4b414414cd1d030d90ad244860f
794934495@qq.com <794934495@qq.com>
2020-11-08 16:24:02 +0800

    冒泡排序

---
+++ BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        boolean flag;
+        for (int i = 0; i < arr.length - 1; i++) {
+            flag = true;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    flag = false;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            if(flag) {
+                return;
+            }
+        }
+    }
+}
