41758bb107f2f27341520ea9f9d96df1e0c12890
794934495@qq.com <794934495@qq.com>
2020-11-08 16:30:22 +0800

    选择排序

--- SelectSort.java
+++ SelectSort.java
@@ -1,10 +1,10 @@
 public class SelectSort {
     //选择排序
     public static void selectSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int min = arr[i];
             int minIndex = i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if(min > arr[j]) {
                     min = arr[j];
                     minIndex = j;
